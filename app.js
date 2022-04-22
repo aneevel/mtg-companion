d91147c6b768d3ae1c3c061782067f726c61dbb2
Alec Neevel <aneevel15@gmail.com>
2022-04-21 23:31:14 -0500

    Add basic listener event

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const express = require('express');
+
+const app = express();
+
+let port = 3000;
+
+// Override in production
+if (process.env.API_PORT) 
+    port = process.env.API_PORT;
+
+
+app.listen(port, () => {
+    console.log(`MTG Companion API listening on port ${port}`);
+});
